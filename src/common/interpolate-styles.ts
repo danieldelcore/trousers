24f3eb9e1abb67c88cc491d0aadca8f78bb5c619
danieldelcore <daniel.delcore.92@gmail.com>
2019-03-13 09:08:32 +1100

    refactor: update code style

--- src/common/interpolate-styles.ts
+++ src/common/interpolate-styles.ts
@@ -4,7 +4,8 @@ export default function interpolateStyles<Theme>(
     theme: Theme,
 ): string {
     return expressions.reduce((result, expression, index) => {
-        const value = typeof expression === 'function' ? expression(theme) : expression;
+        const value =
+            typeof expression === 'function' ? expression(theme) : expression;
 
         return result + value + styles[index + 1];
     }, styles[0]);
--- src/common/is-browser.spec.ts
+++ src/common/is-browser.spec.ts
@@ -6,24 +6,24 @@ import { JSDOM } from 'jsdom';
 
 import { isBrowser } from './';
 
- describe('isBrowser', () => {
+describe('isBrowser', () => {
     it('detect node environment', () => {
         const result = isBrowser();
 
         expect(result).toEqual(false);
     });
 
-     it('detect browser environment', () => {
-         const jsdom = new JSDOM(`<!doctype html><html><body></body></html>`);
-         const { window } = jsdom;
+    it('detect browser environment', () => {
+        const jsdom = new JSDOM(`<!doctype html><html><body></body></html>`);
+        const { window } = jsdom;
 
-         // @ts-ignore
-         global.window = window;
-         // @ts-ignore
-         global.document = window.document;
+        // @ts-ignore
+        global.window = window;
+        // @ts-ignore
+        global.document = window.document;
 
-         const result = isBrowser();
+        const result = isBrowser();
 
-         expect(result).toEqual(true);
-     });
-})
+        expect(result).toEqual(true);
+    });
+});
