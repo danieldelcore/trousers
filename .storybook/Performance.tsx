33a38c132cb102c3cb8b2598c11493a148f1a915
danieldelcore <daniel.delcore.92@gmail.com>
2019-04-12 20:28:23 +1000

    docs: add performance story

---
+++ .storybook/Performance.tsx
@@ -0,0 +1,88 @@
+import { storiesOf } from '@storybook/react';
+import React, { FC, ReactNode, Fragment } from 'react';
+
+import { trousers, useTrousers } from '../src';
+
+storiesOf('Performance', module).add('Many of the same nodes', () => {
+    interface ButtonProps {
+        children: ReactNode;
+    }
+
+    const styles = trousers<ButtonProps, {}, {}>('button').element`
+            background-color: #b3cde8;
+            border-radius: 6px;
+            border: none;
+            box-shadow: inset 0 -4px #9fb8d1;
+            color: white;
+            display: inline-block;
+            cursor: pointer;
+            font-family: sans-serif;
+            font-weight: 500;
+            letter-spacing: 1px;
+            margin: 5px 10px;
+            padding: 10px 20px 14px 20px;
+            text-decoration: none;
+            transition: background-color 300ms, color 300ms;
+            outline: none;
+
+            :hover {
+                background-color: #adc6e0;
+                color: rgba(255, 255, 255, 0.8);
+            }
+
+            :active {
+                background-color: #9fb8d1;
+            }
+        `;
+
+    const Button: FC<ButtonProps> = props => {
+        const buttonClassNames = useTrousers<ButtonProps, {}, {}>(
+            styles,
+            props,
+        );
+
+        return <button className={buttonClassNames}>{props.children}</button>;
+    };
+
+    return (
+        <Fragment>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+            <Button>Hello</Button>
+        </Fragment>
+    );
+});
