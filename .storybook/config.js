b2c4e20630fff836e96ad798a277591c5b4f4f2f
danieldelcore <daniel.delcore.92@gmail.com>
2019-12-14 23:02:01 +1100

    Updated build

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,3 @@
 import { configure } from '@storybook/react';
 
-const req = require.context('../examples', true, /\.tsx$/);
-
-const load = () => {
-    req.keys().forEach(req);
-};
-
-configure(load, module);
+configure(require.context('../examples', true, /\.tsx$/), module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,27 +1,14 @@
 module.exports = ({ config }) => {
     config.module.rules.push({
         test: /\.(ts|tsx)$/,
-        loaders: [{
-            loader: require.resolve('@storybook/addon-storysource/loader'),
-            options: {
-                parser: 'typescript',
-                prettierConfig: {
-                    printWidth: 120,
-                    tabWidth: 2,
-                    bracketSpacing: true,
-                    trailingComma: 'es5',
-                    singleQuote: true,
+        use: [
+            {
+                loader: require.resolve('ts-loader'),
+                options: {
+                    transpileOnly: true,
                 },
             },
-        }],
-        enforce: 'pre',
-    });
-
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        use: [{
-            loader: require.resolve('awesome-typescript-loader')
-        }]
+        ],
     });
 
     config.resolve.extensions.push('.ts', '.tsx');
