c2e1316c7a68f3563e4ebe770608c1835dcf5690
Daniel Del Core <daniel.delcore.92@gmail.com>
2021-01-05 16:19:01 +1100

    ✨ Re-export necessary API from styled, core, react

--- packages/styled/src/index.ts
+++ packages/styled/src/index.ts
@@ -1,1 +1,5 @@
 export { default } from './styled';
+
+// Re-exports necessary functions for standalone use of @trousers/styled
+export { default as css } from '@trousers/core';
+export { default as jsx } from '@trousers/react';
